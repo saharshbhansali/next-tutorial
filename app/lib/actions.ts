7245c2f59eebd6a86a2ddc446cc0babe2efa3fc2
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-11-06 03:32:14 +0530

    Update: finished CREATE route - server actions
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,8 @@
 'use server';
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
  
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -12,11 +15,22 @@ const InvoiceSchema = z.object({
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
   // Test it out:
-  console.log(rawFormData);
+  const test = {customerId, amount, amountInCents, status, date};
+  console.log(test);
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
